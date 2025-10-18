cc9ad41973667bc83c15ef0eaf573f9e66268502
jonathasmaiajorge-gif <jonathasmaiajorge@gmail.com>
2025-10-18 12:39:08 -0300

    feat: Conclui CRUD no Backend (POST, PUT, DELETE) e implementa TarefaService com POO

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { createClient } = require('@supabase/supabase-js');
 
-
+const TarefaService = require('./services/TarefaService');
 dotenv.config();
 
 
@@ -11,6 +11,7 @@ const supabase = createClient(
     process.env.SUPABASE_URL, 
     process.env.SUPABASE_ANON_KEY
 );
+const tarefaService = new TarefaService(supabase);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -40,7 +41,38 @@ app.get('/tarefas', async (req, res) => {
     res.status(200).json(tarefas);
 });
 
+app.post('/tarefas', async (req, res) => {
+    try {
+        
+        const novaTarefa = await tarefaService.criar(req.body); 
+        res.status(201).json(novaTarefa); 
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
 
+app.put('/tarefas/:id', async (req, res) => {
+    try {
+       
+        const id = req.params.id;
+        const tarefaAtualizada = await tarefaService.atualizar(id, req.body);
+        res.status(200).json(tarefaAtualizada);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+app.delete('/tarefas/:id', async (req, res) => {
+    try {
+       
+        const id = req.params.id;
+        const resultado = await tarefaService.deletar(id);
+       
+        res.status(200).json(resultado); 
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
